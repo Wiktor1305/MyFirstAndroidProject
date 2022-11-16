798d5cfb5a6a13e5d476d46bfa3901e450d2cb52
Wiktor1305 <skowronskiwiktor@gmail.com>
2022-11-16 18:59:35 +0100

    hot_lvl_v2(correction)

--- app/src/main/java/com/example/myfirstandroidproject/Hot_lvl.kt
+++ app/src/main/java/com/example/myfirstandroidproject/Hot_lvl.kt
@@ -1,14 +1,29 @@
 package com.example.myfirstandroidproject
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class Hot_lvl : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.peperoni_hot_lvl)
+
+        val alert = AlertDialog.Builder(this@Hot_lvl)
+        alert.setTitle("Uwaga!!!")
+        alert.setMessage("Nasza pizza peperoni jest naprawdę ostra!!!")
+
+        val dymek: AlertDialog = alert.create()
+        dymek.show()
+
+
+        var background = findViewById(R.id.background) as ConstraintLayout
+        background.setBackgroundColor(Color.RED)
     }
 
     fun lvlPiripiri(view: View){
