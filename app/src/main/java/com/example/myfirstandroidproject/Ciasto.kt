6fff98f0718d3996e94ae5442c8a45504a37fe68
Wiktor1305 <skowronskiwiktor@gmail.com>
2022-11-16 17:40:14 +0100

    podsumowanie activity

--- app/src/main/java/com/example/myfirstandroidproject/Ciasto.kt
+++ app/src/main/java/com/example/myfirstandroidproject/Ciasto.kt
@@ -1,11 +1,22 @@
 package com.example.myfirstandroidproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class Ciasto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pizza_ciasto)
+    }
+    fun cienkieCiasto(view: View){
+        val intent = Intent(this, Podsumowanie::class.java)
+        startActivity(intent)
+    }
+
+    fun puszysteCiasto(view: View){
+        val intent = Intent(this, Podsumowanie::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/myfirstandroidproject/Podsumowanie.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstandroidproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Podsumowanie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pizza_podsumowanie)
+
+    }
+}
