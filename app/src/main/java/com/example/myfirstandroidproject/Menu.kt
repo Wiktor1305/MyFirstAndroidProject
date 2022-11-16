4e6a7f1756c1759cc0baf58c7b3c0add2122a6fc
Wiktor1305 <skowronskiwiktor@gmail.com>
2022-11-16 17:51:33 +0100

    android_alertDialog_class

--- app/src/main/java/com/example/myfirstandroidproject/Menu.kt
+++ app/src/main/java/com/example/myfirstandroidproject/Menu.kt
@@ -3,6 +3,8 @@ package com.example.myfirstandroidproject
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 const val EXTRA_MESSAGE="com.example.myfirstandroidproject.MESSAGE"
@@ -15,11 +17,41 @@ class Menu : AppCompatActivity() {
 
     fun sureHawajska(view: View?) {
         //Wykorzystanie klasy Android AlertDialog.
+        val builder = AlertDialog.Builder(this@Menu)
+        builder.setTitle("Wybrano: Pizza Hawajska")
+        builder.setMessage("Jesteś prawdziwym smakoszem pizzy?")
+        builder.setPositiveButton("Nie") { dialog, which ->
+            Toast.makeText(
+                applicationContext,
+                "Włosi nie uznają ananasa na pizzy...",
+                Toast.LENGTH_SHORT
+            ).show()
+
         val menu = "Rodzaj: Pizza Hawajska, "
         val intent = Intent(this, Rozmiar::class.java).apply {
             putExtra(EXTRA_MESSAGE, menu)
         }
         startActivity(intent)
+        }
+
+        builder.setNegativeButton("Tak") { dialog, which ->
+            Toast.makeText(
+                applicationContext,
+                "Prawdziwi smakosze nie uznają ananasa na pizzy. Wybierz inną.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        builder.setNeutralButton("Wróć") { _, _ ->
+            Toast.makeText(
+                applicationContext,
+                "Jesteś niezdecydowany. Wybierz pizze",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
     fun surePeperoni(view: View?) {
